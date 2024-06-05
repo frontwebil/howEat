9ea632a69060ef5b21c2b24ba71fec4a4820ba20
iLyhaha <gollpfd@gmail.com>
2024-06-05 19:19:35 +0300

    let-go

---
+++ main.js
@@ -0,0 +1,63 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// Задаємо початковий час
+let hoursHtml = document.getElementById('hrs');
+let minutesHtml = document.getElementById('min');
+let secondsHtml = document.getElementById('sec');
+
+let hourss = 6;
+let minutess = 45;
+let secondss = 23;
+
+
+const timerInterval = setInterval(() => {
+  secondss--;
+
+  if (secondss < 0) {
+    secondss = 59;
+    minutess--;
+
+    if (minutess < 0) {
+      minutess = 59;
+      hourss--;
+
+      if (hourss < 0) {
+        alert('Час вийшов!');
+        return;
+      }
+    }
+  }
+
+
+  hoursHtml.innerHTML = hourss.toString().padStart(2, 0);
+  minutesHtml.innerHTML = minutess.toString().padStart(2, 0);
+  secondsHtml.innerHTML = secondss.toString().padStart(2, 0);
+}, 1000);
+
+
+const AccordeonTopButton = document.querySelectorAll(".accordeon-top-button");
+
+AccordeonTopButton.forEach((el) => {
+  el.addEventListener("click", () => {
+    const content = el.nextElementSibling;
+    const img = el.lastElementChild;
+    const number = el.firstElementChild.firstElementChild;
+
+    if (content.style.maxHeight) {
+      img.src = "./resault/plus.svg";
+      content.style.maxHeight = null;
+    } else {
+      img.src = "./resault/minus.svg";
+
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
